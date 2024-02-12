893fdbf9a4fb5b8f1d25bf583355f6e5406c41b3
Abdukhakim <rahimboevabduhakim07@gmail.com>
2024-02-13 00:25:09 +0500

    js

--- main.js
+++ main.js
@@ -20,18 +20,6 @@ let product = {
 }
 
 
-// let images = document.querySelectorAll('.shop');
-
-
-// images.forEach((img) => {
-//     if (img.src.includes('shop')) {
-//         img.addEventListener('click', function() {
-//             img.src = './images/done.png';
-//             img.classList.remove('shop')
-//             img.classList.add('shopp')
-//         });
-//     }
-// });
 
 
 
@@ -59,18 +47,55 @@ images.forEach((img) => {
 
 
 
-// let imagess = document.querySelectorAll('.shopp');
+
+let burgersBtns = document.querySelectorAll('.shop')
+    count = document.querySelector('.icoon__count')
+    shopped = document.querySelector('.shopped')
+    icoon = document.querySelector('.icoon')
+    list = document.querySelector('.shopping__list')
+    closee = document.querySelector('.list__top-close')
+    
+   
+    
+shopped.addEventListener('click', () => list.classList.add('active'))  
+closee.addEventListener('click', () => list.classList.remove('active'))
+    
+    
+    
+    
+burgersBtns.forEach((btn) => {
+    btn.addEventListener('click', function() {
+        plus(this)
+    })
+})
 
 
-// imagess.forEach((img) => {
-//     if (img.src.includes('done')) {
-//         img.addEventListener('click', function() {
-//             img.src = './images/shop.png';
-//             img.classList.remove('shopp')
-//             img.classList.add('shop')
-//         });
-//     }
-// });
+function plus(btn) {
+    let parent = btn.closest('.item')
+    let parentId = parent.getAttribute('id')
+    product[parentId].amount++
+    console.log(parentId);
+    
+    basket()
+}
+
+function basket() {
+    for(let key in product) {
+        let allCount  = totalCount()
+        if(allCount > 0) {
+            count.classList.add('active')
+        }else {
+            count.classList.remove('active')
+        }
+        count.innerHTML = allCount
+    }
+}
 
 
-
+function totalCount() {
+    let total = 0;
+    for(let key in product) {
+        total += product[key].amount
+    }
+    return total
+}
